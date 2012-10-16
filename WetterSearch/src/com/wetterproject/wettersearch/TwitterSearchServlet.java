6be28631d952831b466d2c00cc26cd4baedd3d0f
Julian Johansson-Walder <julianjw5@hotmail.com>
2012-10-16 12:00:12 +1000

    Added JSON Skeleton
    -Added JSON skeleton
    -Added in Gson library
    -Added jquery script
    -Refined google search

--- WetterSearch/src/com/wetterproject/wettersearch/TwitterSearchServlet.java
+++ WetterSearch/src/com/wetterproject/wettersearch/TwitterSearchServlet.java
@@ -5,8 +5,13 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+//import com.google.gson.Gson;
+
 @SuppressWarnings("serial")
 public class TwitterSearchServlet extends HttpServlet {
+	
+	//Gson gson = new Gson();
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
@@ -18,5 +23,7 @@ public class TwitterSearchServlet extends HttpServlet {
 protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
     	
 		resp.setContentType("text/plain");
+		
+		
     }
 }
--- WetterSearch/war/Search.jsp
+++ WetterSearch/war/Search.jsp
@@ -5,6 +5,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Google Search</title>
+<!-- Put the following javascript before the closing </head> tag. -->
 <script>
   (function() {
     var cx = '010235793329395389058:8j2oz6hhiwe';
